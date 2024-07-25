cb919fde8ba0e00513985c7274da1a17fbb030b4
Nicholas <gianfrancopr08@gmail.com>
2024-07-25 17:09:08 -0500

    Se guarda el nombre de usuario.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,34 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React, {useState} from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 import Pregunta from './components/Pregunta';
 import HomePage from './components/HomePage';
 import Login from "./components/Login";
-import { Register } from "./components/Register";
+import {Register} from "./components/Register";
 
 const App = () => {
-  const [isAuthenticated, setIsAuthenticated] = useState(false); // Estado de autenticación
-  const [userImage, setUserImage] = useState(''); // Estado para la imagen del usuario
+    const [userName, setUserName] = useState('');
+    const [isAuthenticated, setIsAuthenticated] = useState(false); // Estado de autenticación
+    const [userImage, setUserImage] = useState(''); // Estado para la imagen del usuario
 
-  const handleLogin = (imagePath) => {
-    setIsAuthenticated(true);
-    setUserImage(imagePath); // Guarda la imagen del usuario
-  };
+    const handleLogin = (name, imagePath) => {
+        setIsAuthenticated(true);
+        // Guardar el nombre de usuario
+        setUserName(name);
+        setUserImage(imagePath); // Guarda la imagen del usuario
+    };
 
-  return (
-    <Router>
-      <Routes>
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login onLogin={handleLogin} />} />
-        <Route path="/pregunta" element={isAuthenticated ? <Pregunta userImage={userImage} /> : <HomePage />} />
-        <Route path="/" element={<HomePage />} />
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/register" element={<Register/>}/>
+                <Route path="/login" element={<Login onLogin={handleLogin}/>}/>
+                <Route path="/pregunta"
+                       element={isAuthenticated ? <Pregunta userName={userName} userImage={userImage}/> : <HomePage/>}/>
+                <Route path="/" element={<HomePage/>}/>
+            </Routes>
+        </Router>
+    );
 };
 
 export default App;
