8a1c1e5a0c8a930a0d369d39f8b486832e023f3a
Nicholas <gianfrancopr08@gmail.com>
2024-07-25 12:53:47 -0500

    Pantalla de home terminada.

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -4,22 +4,38 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const HomePage = () => {
     return (
-        <Container className="mt-4">
-            <Row className="justify-content-center">
-                <Col md={6}>
-                    <h1 className="text-center mb-4">Bienvenido a AskMeInformMe</h1>
-                    <p className="text-center mb-4">
-                        ¡Bienvenido a nuestra aplicación de trivia! Aquí podrás jugar a un emocionante juego de
-                        preguntas y respuestas.
+        <Container className="d-flex flex-column justify-content-center align-items-center vh-100">
+            <Row className="text-center mb-4">
+                <Col>
+                    <h1 className="display-4 mb-2">Bienvenido a AskMeInformMe</h1>
+                    <h2 className="display-5 text-muted">¡El mejor lugar para probar tus conocimientos!</h2>
+                </Col>
+            </Row>
+            <Row className="text-center mb-4">
+                <Col>
+                    <img
+                        src="/images/imagenhome.jpeg"
+                        alt="Welcome"
+                        style={{maxWidth: '40%', height: 'auto'}}
+                    />
+                </Col>
+            </Row>
+            <Row className="text-center mb-4">
+                <Col>
+                    <p className="lead">
+                        ¡Prepárate para un desafío emocionante con nuestra aplicación de trivia! Compite con amigos y
+                        demuestra tus conocimientos en una variedad de categorías.
                     </p>
-                    <div className="text-center">
-                        <Button color="primary" href="/register" className="me-2">
-                            Registrarse
-                        </Button>
-                        <Button color="secondary" href="/login">
-                            Iniciar sesión
-                        </Button>
-                    </div>
+                </Col>
+            </Row>
+            <Row className="text-center">
+                <Col>
+                    <Button color="primary" size="lg" className="me-3 mb-3" href="/register">
+                        Registrarse
+                    </Button>
+                    <Button color="success" size="lg" className="mb-3" href="/login">
+                        Iniciar sesión
+                    </Button>
                 </Col>
             </Row>
         </Container>
