5d502afafd3a1dcee86b2838efd36c94b4cbdb51
Nicholas <gianfrancopr08@gmail.com>
2024-07-24 21:25:48 -0500

    Actualización rutas.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,30 @@
-import React from 'react';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Pregunta from './components/Pregunta';
 import HomePage from './components/HomePage';
 import Login from "./components/Login";
-import {Register} from "./components/Register";
+import { Register } from "./components/Register";
 
 const App = () => {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/register" element={<Register/>}/>
-                <Route path="/login" element={<Login/>}/>
-                <Route path="/pregunta" element={<Pregunta/>}/>
-                <Route path="/" element={<HomePage/>}/>
-            </Routes>
-        </Router>
-    );
+  const [isAuthenticated, setIsAuthenticated] = useState(false); // Estado de autenticación
+  const [userImage, setUserImage] = useState(''); // Estado para la imagen del usuario
+
+  const handleLogin = (imagePath) => {
+    setIsAuthenticated(true);
+    setUserImage(imagePath); // Guarda la imagen del usuario
+  };
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login onLogin={handleLogin} />} />
+        <Route path="/pregunta" element={isAuthenticated ? <Pregunta userImage={userImage} /> : <HomePage />} />
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
