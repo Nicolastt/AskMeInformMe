844ed18b36bc8f9ad8ef14365ba4bf16ff54beb5
Nicholas <gianfrancopr08@gmail.com>
2024-07-24 21:20:44 -0500

    Actualización del index

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Pregunta from "./Pregunta";
+import Pregunta from "./components/Pregunta";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <Pregunta/>
+        <App/>
     </React.StrictMode>
 );
 
