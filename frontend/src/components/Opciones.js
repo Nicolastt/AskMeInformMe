5b22eac4e066cb6f151f390d59e8bd2d4ee52278
Nicholas <gianfrancopr08@gmail.com>
2024-07-26 18:19:23 -0500

    Componente opciones con colores.

---
+++ frontend/src/components/Opciones.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Col, Button, Row } from 'reactstrap';
+import '../css/Opciones.css'; // Asegúrate de que la ruta sea correcta
+
+const Opciones = ({ opciones, respuestaSeleccionada, handleRespuestaSeleccionada, respuestaCorrecta }) => {
+    return (
+        <Row>
+            {opciones.map((opcion, index) => (
+                <Col xs="6" className="mb-3" key={index}>
+                    <Button
+                        className={`opcion-button opcion-button-${index + 1} ${respuestaSeleccionada && respuestaSeleccionada.ID === opcion.ID ? '' : ''}`}
+                        onClick={() => handleRespuestaSeleccionada(opcion)}
+                        disabled={respuestaCorrecta !== null}
+                        block
+                    >
+                        <span className="opcion-text">{String.fromCharCode(65 + index)}. {opcion.Texto}</span>
+                    </Button>
+                </Col>
+            ))}
+        </Row>
+    );
+};
+
+export default Opciones;
