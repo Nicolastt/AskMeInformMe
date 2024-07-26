8c4a7b47e17e5327593b480ebf7d87785b5741fb
Nicholas <gianfrancopr08@gmail.com>
2024-07-25 21:30:21 -0500

    Ya guarda los puntajes altos.

--- frontend/src/components/Score.js
+++ frontend/src/components/Score.js
@@ -11,6 +11,32 @@ const Score = ({ userName, userImage, puntaje, totalTime }) => {
     const maxScore = 130;
     const scorePercentage = (puntaje / maxScore) * 100;
 
+    const saveScore = async () => {
+        try {
+            const response = await fetch('/save_score', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    userName: userName,
+                    puntaje: puntaje,
+                    totalTime: totalTime
+                }),
+            });
+
+            if (!response.ok) {
+                const errorData = await response.json();
+                console.error('Error al guardar el puntaje:', errorData.error);
+            } else {
+                console.log('Puntaje guardado correctamente');
+                window.location.href = '/';
+            }
+        } catch (error) {
+            console.error('Error en la solicitud:', error);
+        }
+    };
+
     return (
         <Container className="mt-4 score-container">
             <Row className="mb-4 justify-content-center">
@@ -44,14 +70,13 @@ const Score = ({ userName, userImage, puntaje, totalTime }) => {
                                 <Progress color="success" value={scorePercentage} />
                                 <p className="progress-text">{puntaje} / {maxScore} puntos</p>
                             </div>
-                            <Button color="success" href="/" className="mt-3 ms-2">
+                            <Button color="success" className="mt-3 ms-2" onClick={saveScore}>
                                 Volver al inicio
                             </Button>
                         </CardBody>
                     </Card>
                 </Col>
             </Row>
-
         </Container>
     );
 };
